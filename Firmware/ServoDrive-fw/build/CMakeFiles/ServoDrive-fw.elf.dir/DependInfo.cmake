2a07720a0755cacafac1d473ed4b3333fcf3c14e
sylvain-yu <jasonshake@163.com>
2022-08-10 13:12:42 +0800

    support for vscode cmake

---
+++ Firmware/ServoDrive-fw/build/CMakeFiles/ServoDrive-fw.elf.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/startup/startup_stm32f042x6.s" "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/build/CMakeFiles/ServoDrive-fw.elf.dir/startup/startup_stm32f042x6.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F0"
+  "STM32F042x6"
+  "STM32F0xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Inc"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/CMSIS/Include"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Ctrl"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/UserApp"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/adc.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/adc.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/adc.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/dma.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/dma.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/dma.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/flash.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/flash.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/flash.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/gpio.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/i2c.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/i2c.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/i2c.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/main.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/main.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/stm32f0xx_hal_msp.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/stm32f0xx_hal_msp.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/stm32f0xx_hal_msp.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/stm32f0xx_it.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/stm32f0xx_it.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/stm32f0xx_it.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/syscalls.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/system_stm32f0xx.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/system_stm32f0xx.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/system_stm32f0xx.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Src/tim.c" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Core/Src/tim.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc_ex.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc_ex.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Ctrl/motor.cpp" "CMakeFiles/ServoDrive-fw.elf.dir/Ctrl/motor.cpp.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Ctrl/motor.cpp.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Ctrl/random_flash_utils.cpp" "CMakeFiles/ServoDrive-fw.elf.dir/Ctrl/random_flash_utils.cpp.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/Ctrl/random_flash_utils.cpp.obj.d"
+  "D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/UserApp/main.cpp" "CMakeFiles/ServoDrive-fw.elf.dir/UserApp/main.cpp.obj" "gcc" "CMakeFiles/ServoDrive-fw.elf.dir/UserApp/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Firmware/ServoDrive-fw/build/CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj.d
@@ -0,0 +1,39 @@
+CMakeFiles/ServoDrive-fw.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj: \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Core/Inc/stm32f0xx_hal_conf.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f042x6.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/CMSIS/Include/core_cm0.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\9\ 2019-q4-major\lib\gcc\arm-none-eabi\9.2.1\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\9\ 2019-q4-major\arm-none-eabi\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\9\ 2019-q4-major\arm-none-eabi\include\machine\_default_types.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\9\ 2019-q4-major\arm-none-eabi\include\sys\features.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\9\ 2019-q4-major\arm-none-eabi\include\_newlib_version.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\9\ 2019-q4-major\arm-none-eabi\include\sys\_intsup.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\9\ 2019-q4-major\arm-none-eabi\include\sys\_stdint.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/CMSIS/Include/cmsis_version.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/CMSIS/Include/cmsis_compiler.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\9\ 2019-q4-major\lib\gcc\arm-none-eabi\9.2.1\include\stddef.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_exti.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim.h \
+ D:/CloneBot/Clone_ElectronBot/Firmware/ServoDrive-fw/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h
